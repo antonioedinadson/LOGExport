3870d603a9fa76ddbb5bac8dc4ed80cf086d9893
Antônio Edinadson Gomes Eugênio <antonioedinadson@gmail.com>
2021-12-28 00:35:27 -0300

    Adicionar arquivos de projeto.

---
+++ LOGExport/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace LOGExport {
+    partial class Form1 {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BTNOpen = new System.Windows.Forms.Button();
+            this.BTNLoad = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.textBox1.Location = new System.Drawing.Point(25, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(490, 19);
+            this.textBox1.TabIndex = 0;
+            // 
+            // BTNOpen
+            // 
+            this.BTNOpen.Location = new System.Drawing.Point(531, 14);
+            this.BTNOpen.Name = "BTNOpen";
+            this.BTNOpen.Size = new System.Drawing.Size(133, 35);
+            this.BTNOpen.TabIndex = 1;
+            this.BTNOpen.Text = "OPEN LIST";
+            this.BTNOpen.UseVisualStyleBackColor = true;
+            this.BTNOpen.Click += new System.EventHandler(this.BTNOpen_Click);
+            // 
+            // BTNLoad
+            // 
+            this.BTNLoad.Location = new System.Drawing.Point(683, 14);
+            this.BTNLoad.Name = "BTNLoad";
+            this.BTNLoad.Size = new System.Drawing.Size(133, 35);
+            this.BTNLoad.TabIndex = 2;
+            this.BTNLoad.Text = "LOAD LIST";
+            this.BTNLoad.UseVisualStyleBackColor = true;
+            this.BTNLoad.Click += new System.EventHandler(this.BTNLoad_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(25, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(791, 290);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(838, 387);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.BTNLoad);
+            this.Controls.Add(this.BTNOpen);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BTNOpen;
+        private System.Windows.Forms.Button BTNLoad;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ LOGExport/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOGExport {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        public string filePath;
+
+        private void BTNOpen_Click(object sender, EventArgs e) {
+            
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK) {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+                filePath = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void BTNLoad_Click(object sender, EventArgs e) {
+
+            string[] dir = Directory.GetFiles(textBox1.Text, "*.*", SearchOption.AllDirectories);
+
+            foreach (var item in dir) {
+                Console.WriteLine(item);
+            }
+           
+        }
+
+        
+    }
+}
